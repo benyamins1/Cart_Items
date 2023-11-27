f946dac4f8506317d5a3886b09af631bc2db0249
benyamin <benyamins1@gmail.com>
2023-11-27 20:55:49 +0200

    srv

--- cart_front/src/services/product-services.js
+++ cart_front/src/services/product-services.js
@@ -1,38 +1,42 @@
 import axios from 'axios';
 class ProductDataService {
+    
+        axe="https://django1-txc8.onrender.com"
+    
     login(data) {
-        return axios.post("http://localhost:8000/token/", data);
+        return axios.post(`${this.axe}/token/`, data);
     }
 
     signup(data) {
-        return axios.post("http://localhost:8000/register/", data);
+        return axios.post(`${this.axe}/register/`, data);
     }
     getAll() {
         // axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-        return axios.get('http://localhost:8000/products/');
+        console.log(`${this.axe}/products/`)
+        return axios.get(`${this.axe}/products/`);
     }
     getCart(token) {
         axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-        return axios.get('http://localhost:8000/carts/');
+        return axios.get(`${this.axe}/carts/`);
     }
     async updateCartItem(id, data, token) {
         axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-        return axios.put(`http://localhost:8000/cartitems/${id}/`, data);
+        return axios.put(`${this.axe}/cartitems/${id}/`, data);
     }
 
      deleteCartItem(id, token) {
         axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-        return axios.delete(`http://localhost:8000/cartitems/${id}/`);
+        return axios.delete(`${URL}/cartitems/${id}/`);
     }
 
      createCartItem(data, token) {
         axios.defaults.headers.common["Authorization"] = "Bearer " + token;
         data.product_id = data.product.id
-        return axios.post("http://localhost:8000/cartitems/", data);
+        return axios.post(`${this.axe}/cartitems/`, data);
     }
     async payCart(data, token) {
         axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-        return axios.patch(`http://localhost:8000/carts/`, data);
+        return axios.patch(`${this.axe}/carts/`, data);
     }
 }
 
